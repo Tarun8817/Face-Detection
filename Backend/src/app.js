15cea0b488995a5bd5a14d0cadf67010d0333386
Tarun8817 <rajputtarun698@gmail.com>
2026-05-08 22:47:35 +0530

    fix routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+const authRoutes = require("./routes/auth.router")
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+/**
+ * Routes
+*/
+app.use("/api/auth",authRoutes)
 
 module.exports = app;
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const userModel = require("../models/user.model");
+const userModel = require("../models/user.models.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -35,16 +35,18 @@ async function registerUser(req, res) {
             }
         );
 
-        res.cookie("token",token)
+        res.cookie("token", token);
 
         res.status(201).json({
             message: "User registered successfully",
-            user:{
-                id:user._id,
-                username:user.username,
-                email:user.email
-            }
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+            token,
         });
+
     } catch (error) {
         res.status(500).json({
             message: error.message,
@@ -52,21 +54,59 @@ async function registerUser(req, res) {
     }
 }
 
+async function loginUser(req, res) {
+    try {
+        const { email, password, username } = req.body;
 
-async function loginUser(req,res){
-    const{email,password,username} = req.body;
+        const user = await userModel.findOne({
+            $or: [{ email }, { username }],
+        });
 
-    const user = await userModel.findOne({
-        $or:[
-            {email},{username}
-        ]
-    })
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
 
-    if(!user){
-        return res.status(400).json({
-            message:"Invalid credentials"
-        })
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "3d",
+            }
+        );
+
+        res.cookie("token", token);
+
+        res.status(200).json({
+            message: "Login successful",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+            token,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
     }
 }
 
-module.exports = { registerUser };
+module.exports = {
+    registerUser,
+    loginUser,
+};
